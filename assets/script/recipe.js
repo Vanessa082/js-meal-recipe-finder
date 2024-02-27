438edb72e0f3f82db006d2a1a8b3542529f33fe3
vanessa082 <wahvanessa22@gmail.com>
2024-02-27 02:43:41 +0100

    wrote the html code for the newly created recipe.html page added styling and the js functionalities which was to fetch data of every item on the page

---
+++ assets/script/recipe.js
@@ -0,0 +1,73 @@
+const searchParams = new URLSearchParams(window.location.search)
+const ingredientsMeasurement = document.querySelector('.ingredients-Measurement')
+const mealImage = document.querySelector('.strMealThumb')
+const mealName = document.querySelector('.strMeal')
+const mealCategory = document.querySelector('.strCategory')
+const mealArea = document.querySelector('.strArea')
+const sourceButton = document.querySelector('.strSource')
+const youtubeButton = document.querySelector('.strYoutube')
+const mealInstructions = document.querySelector('.instructions')
+
+const idMeal = searchParams.get('idMeal')
+
+
+
+
+if (idMeal) {
+  // fetch for meal with that id.
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`)
+    .then((response) => response.json())
+    .then((data) => displayMealDetails(data.meals[0]))
+    .catch((error) => console.error('Error fetching data:', error));
+}
+
+const displayMealDetails = (meal) => {
+  const mealThumbnail = meal.strMealThumb
+  mealImage.src = mealThumbnail;
+  mealImage.alt = meal.strMeal;
+
+  mealName.innerHTML = meal.strMeal
+  mealCategory.innerHTML = meal.strCategory
+  mealArea.innerHTML = meal.strArea
+
+  const listOfIngredients = document.querySelector('.listOfIngredients')
+  const listOfMeasurements = document.querySelector('.listOfMeasurements')
+
+  for (let i = 1; i <= 20; i++) {
+    const ingredient = meal[`strIngredient${i}`]
+    const measurement = meal[`strMeasure${i}`]
+
+    if (ingredient) {
+      const ingredientItem = document.createElement('li')
+      ingredientItem.textContent = ingredient
+      listOfIngredients.appendChild(ingredientItem)
+    }
+
+    if (measurement) {
+      const measurementItem = document.createElement('li')
+      measurementItem.textContent = measurement
+      listOfMeasurements.appendChild(measurementItem)
+    }
+  }
+
+
+  const instructionsList = document.createElement('ol')
+  const instructionsSteps = meal.strInstructions.split('\r\n\r\n')
+
+  instructionsSteps.forEach(step => {
+    const listIterm = document.createElement('li')
+    listIterm.textContent = step
+    instructionsList.appendChild(listIterm)
+  })
+
+  mealInstructions.appendChild(instructionsList)
+
+
+  const mealYutubeLink = meal.strYoutube
+  const mealSourceLink = meal.strSource
+
+  youtubeButton.href = mealYutubeLink
+  sourceButton.href = mealSourceLink
+
+}
+console.log('id to fetch', idMeal) 
